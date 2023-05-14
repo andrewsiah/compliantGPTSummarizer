59469599460dc68f212190e1c0c6184f0ecdf661
Andrew Siah <andrew.siah@abra.com>
2023-05-14 15:19:53 -0400

    added a frontend

--- home.py
+++ home.py
@@ -10,6 +10,52 @@ st.set_page_config(
 )
 
 
+from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
+from langchain.embeddings import LlamaCppEmbeddings
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.vectorstores import Chroma
+from langchain.llms import GPT4All, LlamaCpp
+import os
+
+load_dotenv()
+
+llama_embeddings_model = os.environ.get("LLAMA_EMBEDDINGS_MODEL")
+persist_directory = os.environ.get('PERSIST_DIRECTORY')
+
+model_type = os.environ.get('MODEL_TYPE')
+model_path = os.environ.get('MODEL_PATH')
+model_n_ctx = os.environ.get('MODEL_N_CTX')
+
+from constants import CHROMA_SETTINGS
+
+def main():
+    llama = LlamaCppEmbeddings(model_path=llama_embeddings_model, n_ctx=model_n_ctx)
+    db = Chroma(persist_directory=persist_directory, embedding_function=llama, client_settings=CHROMA_SETTINGS)
+    retriever = db.as_retriever()
+    # Prepare the LLM
+    callbacks = [StreamingStdOutCallbackHandler()]
+    match model_type:
+        case "LlamaCpp":
+            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False)
+        case "GPT4All":
+            llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', callbacks=callbacks, verbose=False)
+        case _default:
+            print(f"Model {model_type} not supported!")
+            exit;
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+    # Interactive questions and answers
+
+    # Get the answer from the chain
+    res = qa("Give me a summary of emails from techinnovate.com only.")    
+    answer, docs = res['result'], res['source_documents']
+    return answer
+
+answer = main()
+
+
+
 st.title("CompliantGPT")
 st.button("Click to summarize your emails in source documents.")
+st.write(answer)
 
